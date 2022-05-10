776acf59fb66ab93ce93c54f0661ef4e7765a62d
João Felipe P <jf.pelliccione@gmail.com>
2022-05-10 09:19:30 -0300

    Conclusão do exercício 4.

--- Bloco_33/33_2-4.py
+++ Bloco_33/33_2-4.py
@@ -2,8 +2,7 @@
 livros em cada categoria, sempre com relação ao número total de livros.
 O resultado deve ser escrito em um arquivo no formato CSV."""
 import json
-
-categories_from_books = []
+import csv
 
 
 def retrieve_books():
@@ -16,13 +15,30 @@ def count_books_by_categories(books):
     for book in books:
         for category in book["categories"]:
             if not categories.get(category):
-                categories[category] = 0
+                categories[category] = 1
             else:
                 categories[category] += 1
 
     return categories
 
 
-books_arr = retrieve_books()
-categories_dict = count_books_by_categories(books_arr)
-print(categories_dict)
+def build_csv_report():
+    books_arr = retrieve_books()
+    num_of_books = len(books_arr)
+    categories_dict = count_books_by_categories(books_arr)
+    headers = ["categoria", "porcentagem"]
+
+    with open("./Assets/report.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+
+        for category in categories_dict:
+            percentage = categories_dict[category] / num_of_books
+            writer.writerow([category, percentage])
+
+
+books = retrieve_books()
+cat_dict = count_books_by_categories(books)
+print(cat_dict)
+
+build_csv_report()
