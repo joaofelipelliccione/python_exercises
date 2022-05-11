9b47d5999bf6dd21cbe5dcfac7c731e9c0f18d11
João Felipe P <jf.pelliccione@gmail.com>
2022-05-11 18:47:28 -0300

    Conclusão do exercício 9.

--- Bloco_35/35_3-6.py
+++ Bloco_35/35_3-6.py
@@ -11,9 +11,21 @@ from pymongo import MongoClient
 def populate_db():
     with open("./Assets/books.json") as file:
         books_arr = json.load(file)
-        with MongoClient() as client:
+        with MongoClient("mongodb://localhost:27017/") as client:
             db = client.library
             db.books.insert_many(books_arr)
 
 
-populate_db()
+"""Chame essa função, caso deseje criar e popular
+a coleção books, do banco library."""
+# populate_db()
+
+
+def get_book_titles_from_category(category):
+    with MongoClient("mongodb://localhost:27017/") as client:
+        db = client.library
+        for book in db.books.find({"categories": category}):
+            print(book["title"])
+
+
+print(get_book_titles_from_category("Internet"))
