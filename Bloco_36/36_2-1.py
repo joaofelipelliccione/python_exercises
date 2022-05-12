0ecb0205de0dbedb13fff26a9231487af2fae6c6
João Felipe P <jf.pelliccione@gmail.com>
2022-05-12 09:36:46 -0300

    Conclusão do exercício 12.

---
+++ Bloco_36/36_2-1.py
@@ -0,0 +1,14 @@
+"""
+Faça um algoritmo recursivo de soma. Esse, deve receber um N° como argumento
+para, então, somar todos os números antecessores a ele.
+"""
+
+
+def recursive_sum(num):
+    if num == 0:
+        return 0
+    else:
+        return num + recursive_sum(num - 1)
+
+
+print(recursive_sum(4))  # 10
