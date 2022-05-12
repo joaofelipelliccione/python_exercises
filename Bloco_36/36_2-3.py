a026d0f3ebad5f8f830e393b8d96d0622c8e2f81
João Felipe P <jf.pelliccione@gmail.com>
2022-05-12 17:10:36 -0300

    Conclusão do exercício 14.

---
+++ Bloco_36/36_2-3.py
@@ -0,0 +1,15 @@
+"""Faça uma função recursiva que recebe um array 
+e o retorna na ordem reversa."""
+
+
+def reverse_arr(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        print(arr[1:])
+        print([arr[0]])
+        return reverse_arr(arr[1:]) + [arr[0]]
+
+
+test_arr = ["A", "B", "C", "D"]
+print(reverse_arr(test_arr))
