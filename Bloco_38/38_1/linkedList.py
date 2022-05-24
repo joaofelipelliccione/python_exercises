e6caec66c0700f7ce232e1623b4e856cae1def3e
João Felipe P <jf.pelliccione@gmail.com>
2022-05-24 18:16:25 -0300

    Conclusão da estrutura da LinkedList!

--- Bloco_38/38_1/linkedList.py
+++ Bloco_38/38_1/linkedList.py
@@ -46,6 +46,14 @@ class LinkedList:
         actual_last_value.next = new_last_value
         self.__length += 1
 
+    def remove_first(self):
+        value_to_be_removed = self.head_value
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
+            self.__length -= 1
+        return value_to_be_removed
+
     def remove_last(self):
         if len(self) <= 1:
             return self.remove_first()
@@ -85,3 +93,36 @@ class LinkedList:
 
     def is_empty(self):
         return not self.__length
+
+
+linked_list = LinkedList()
+
+print(linked_list.is_empty())  # saída: True
+linked_list.insert_first(1)
+print(linked_list)  # saída: LinkedList(len=1 value=Node(value=1 next=None))
+
+linked_list.insert_first(2)
+print(
+    linked_list
+)  # saída: LinkedList(len=2 value=Node(value=2 next=Node(value=1 next=None)))
+
+linked_list.insert_last(3)
+print(
+    linked_list
+)  # saída: LinkedList(len=3 value=Node(value=2 next=Node(value=1 next=Node(value=3 next=None))))
+
+linked_list.remove_last()
+print(
+    linked_list
+)  # saída: LinkedList(len=2 value=Node(value=2 next=Node(value=1 next=None)))
+
+linked_list.remove_first()
+print(linked_list)  # saída: LinkedList(len=1 value=Node(value=1 next=None))
+
+linked_list.insert_at(5, 1)
+print(
+    linked_list
+)  # saída: LinkedList(len=2 value=Node(value=1 next=Node(value=5 next=None)))
+
+linked_list.remove_at(0)
+print(linked_list)  # saída: LinkedList(len=1 value=Node(value=5 next=None))
